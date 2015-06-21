3cee6aaa3235136a9dc075ab3798232e738b7522
dfirstov <dfirstov@i-novus.ru>
2015-06-21 23:11:26 +0300

    [NF] Рекурсивный поиск подкаталогов

--- src/main/java/ru/firstquad/algorithm/func/Recursive.java
+++ src/main/java/ru/firstquad/algorithm/func/Recursive.java
@@ -1,6 +1,8 @@
 package ru.firstquad.algorithm.func;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,8 +13,23 @@ public class Recursive {
 
     public static List<String> getDirs(String path, List<String> dirs) {
         File file = new File(path);
-        if (file.isDirectory())
-            file.
-        return null;
+        if (file.isDirectory()) {
+            dirs.addAll(getOnlyDirs(file));
+            String[] parent = path.split(file.getName());
+            if (parent.length > 0)
+                getDirs(parent[0], dirs);
+        }
+        return dirs;
+    }
+
+    private static List<String> getOnlyDirs(File file) {
+        if (file == null)
+            return null;
+        List<String> dirs = new ArrayList<String>();
+        for (File file1 : file.listFiles()) {
+            if (file1.isDirectory())
+                dirs.add(file1.getName());
+        }
+        return dirs;
     }
 }
