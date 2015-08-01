43a100a1e2458852a2d6fecf014c1b2074011a90
dfirstov <dfirstov@i-novus.ru>
2015-08-01 12:27:04 +0300

    [NF] Рекурсивное удаление пустых подкаталогов

--- src/main/java/ru/firstquad/algorithm/func/Recursive.java
+++ src/main/java/ru/firstquad/algorithm/func/Recursive.java
@@ -1,7 +1,9 @@
 package ru.firstquad.algorithm.func;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,5 +32,40 @@ public class Recursive {
                 dirs.add(file1.getName());
         }
         return dirs;
+    }
+
+    public static void deleteEmptyDirs(File root, File dir, ArrayList<File> checkedDirs) throws IOException {
+        if (checkedDirs.contains(root)) {
+            return;
+        }
+        File[] childs = dir.listFiles();
+        if (childs == null || childs.length == 0) {
+            File parentFile = dir.getParentFile();
+            dir.delete();
+            if (parentFile != null) {
+                deleteEmptyDirs(root, dir.getParentFile(), checkedDirs);
+                return;
+            }
+        }
+        if (checkedDirs.containsAll(Arrays.asList(childs)) && dir.getParentFile() != null) {
+            checkedDirs.add(dir);
+            deleteEmptyDirs(root, dir.getParentFile(), checkedDirs);
+        } else {
+            for (File child : childs) {
+                if (checkedDirs.contains(child)) {
+                    continue;
+                }
+                if (child.isDirectory() && (child.listFiles() == null || child.listFiles().length == 0)) {
+                    File parentFile = child.getParentFile();
+                    if (child.exists())
+                        child.delete();
+                    deleteEmptyDirs(root, parentFile, checkedDirs);
+                } else if (child.isFile()) {
+                    checkedDirs.add(child.getParentFile());
+                } else {
+                    deleteEmptyDirs(root, child, checkedDirs);
+                }
+            }
+        }
     }
 }
