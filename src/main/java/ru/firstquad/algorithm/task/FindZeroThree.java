3648df19e8208caddbc376277cd9914780f5655d
dmitriy <firstquad@mail.ru>
2016-09-22 14:29:33 +0300

    [NF] add FindZeroThree

--- src/main/java/ru/firstquad/algorithm/task/FindZeroThree.java
+++ src/main/java/ru/firstquad/algorithm/task/FindZeroThree.java
@@ -2,44 +2,44 @@ package ru.firstquad.algorithm.task;
 
 import ru.firstquad.algorithm.search.impl.BinarySearch;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
- * Created by dmitriy on 17/09/16.
+ * Created by dmitriy on 19/09/16.
  */
-public class FindZeroPairs {
+public class FindZeroThree {
 
     public static void main(String[] args) {
-        Integer[] in = IntStream.range(-10000, 10000).boxed().toArray(Integer[]::new);
+        Integer[] in = IntStream.range(-1000, 1000).boxed().toArray(Integer[]::new);
         long start = System.currentTimeMillis();
-        countQuadratic(in);
+        countNaive(in);
         System.out.println(System.currentTimeMillis() - start);
         start = System.currentTimeMillis();
-        countBinary(in);
+        countLog(in);
         System.out.println(System.currentTimeMillis() - start);
     }
 
-    private static void countQuadratic(Integer[] in) {
+    private static void countNaive(Integer[] in) {
         int count = 0;
         for (int i = 0; i < in.length; i++) {
             for (int j = i + 1; j < in.length; j++) {
-                if (in[i] + in[j] == 0)
-                    count++;
+                for (int k = j + 1; k < in.length; k++) {
+                    if (in[i] + in[j] + in[k] == 0)
+                        count++;
+                }
             }
         }
         System.out.println(count);
     }
 
-    private static void countBinary(Integer[] in) {
-        Arrays.sort(in);
+    private static void countLog(Integer[] in) {
         int count = 0;
         for (int i = 0; i < in.length; i++) {
-            if (new BinarySearch().search(in, -in[i]).compareTo(i) > 0)
-                count++;
+            for (int j = i + 1; j < in.length; j++) {
+                if (new BinarySearch<>().search(in, -in[i] - in[j]) > j)
+                    count++;
+            }
         }
         System.out.println(count);
     }
-
-
 }
